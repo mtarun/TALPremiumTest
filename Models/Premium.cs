b223f4d5478b15e07d3c2e7454fb2500d088a92f
mtarun <mtarun@hotmail.com>
2020-10-11 21:37:09 +1100

    Added more html and calculation logic
    Added more html and calculation logic

---
+++ Models/Premium.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TalPremiumTest.Models
+{
+    public class Premium
+    {
+        public string Name { get; set; }
+
+        public decimal Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string Occupation { get; set; }
+
+        public decimal DeathSumInsured { get; set; }
+
+    }
+}
--- Scripts/premiumapp.js
+++ Scripts/premiumapp.js
@@ -3,5 +3,40 @@
         $scope.calculated = false;
         $scope.Calculate = function (premium) {
             $scope.calculated = true;
+
+            $scope.$watch('formCalculate.$valid', function (newValue) {
+                $scope.isFormValid = newValue;
+            });
+
+            if ($scope.isFormValid) {
+                $scope.changedValue(premium);
+            }
+        }
+        $scope.arrlist = [{
+            "rating": "Light Manual",
+            "occupation": "Cleaner"
+        }, {
+            "rating": "Professional",
+            "occupation": "Doctor"
+        }, {
+            "rating": "White Collar",
+            "occupation": "Author"
+        }, {
+            "rating": "Heavy Manual",
+            "occupation": "Farmer"
+        }, {
+            "rating": "Heavy Manual",
+            "occupation": "Mechanic"
+        }, {
+            "rating": "Light Manual",
+            "occupation": "Florist"
+        }];
+        occupationRating = { "Professional": 1.0, "White Collar": 1.25, "Light Manual": 1.50, "Heavy Manual": 1.75 };
+        $scope.changedValue = function (item) {
+            if ($scope.isFormValid) {
+                $scope.calculation = ((item.DeathSumInsured * occupationRating[item.Occupation] * item.Age) / 1000) * 12;
+            }
         }
     });
+
+
