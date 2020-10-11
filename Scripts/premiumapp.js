cffe3d31204ecc0566ad1199dd16b1b4f01e330a
mtarun <mtarun@hotmail.com>
2020-10-11 21:11:00 +1100

    add controller and form field
    added controller and first field

--- Scripts/premiumapp.js
+++ Scripts/premiumapp.js
@@ -1,4 +1,7 @@
 ﻿angular.module("PremiumApp", [])
     .controller("HomeController", function ($scope) {
-
-    }
+        $scope.calculated = false;
+        $scope.Calculate = function (premium) {
+            $scope.calculated = true;
+        }
+    });
